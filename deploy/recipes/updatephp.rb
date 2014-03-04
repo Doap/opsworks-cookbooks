bdc95a4f69f2fd56b4ffd8f0eba714bc672aa632
skc4192 <skc4192@yahoo.com>
2014-03-04 12:14:20 -0600

    Update updatephp.rb

--- deploy/recipes/updatephp.rb
+++ deploy/recipes/updatephp.rb
@@ -8,8 +8,7 @@ node[:deploy].each do |application, deploy|
    execute 'Change Default File Upload Size' do
        action :run
        command "
-          sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 50M/g'
-          `php -r 'echo php_ini_loaded_file();'`
+          sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 50M/g' `php -r 'echo php_ini_loaded_file();'`
        "
        user 'root'
        notifies :reload, resources(:service => "apache2"), :delayed
