7ef1d0ab04136f5c49ad4d7b4550b22e883657e5
skc4192 <skc4192@yahoo.com>
2014-03-04 12:28:36 -0600

    Update php.rb

--- mod_php5_apache2/recipes/php.rb
+++ mod_php5_apache2/recipes/php.rb
@@ -57,14 +57,4 @@ node[:deploy].each do |application, deploy|
       ::File.exists?("#{node[:apache][:dir]}/sites-enabled/000-default")
     end
   end
-
-  execute 'Change Default File Upload Size' do
-       action :run
-       command "
-          sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 50M/g'
-          `php -r 'echo php_ini_loaded_file();'`
-       "
-       user 'root'
-       notifies :reload, resources(:service => "apache2"), :delayed
-   end
 end
