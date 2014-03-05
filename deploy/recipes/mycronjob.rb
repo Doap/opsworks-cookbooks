cb8b3aaf2b21d516cb1292d4785978f0a4e148c7
skc4192 <skc4192@yahoo.com>
2014-03-05 09:26:05 -0600

    Update mycronjob.rb

--- deploy/recipes/mycronjob.rb
+++ deploy/recipes/mycronjob.rb
@@ -2,10 +2,9 @@ node[:deploy].each do |application, deploy|
    execute 'Add My Cron Job' do
        action :run
        command <<-EOT
-          command="date >> /var/log/mylog.txt"
-		   job="5 * * * * $command"
-	           cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
-
+       command="date >> /var/log/mylog.txt"
+       job="5 * * * * $command"
+       cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
        EOT
        user 'root'
    end
